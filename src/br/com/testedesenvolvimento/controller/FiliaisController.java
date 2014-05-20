453529ba08d9442a2c4d3d30b5d254e9c0624bb6
Victor Fernando de Oliveira Sampaio <victorfsampaio@gmail.com>
2014-05-20 13:50:54 -0300

    Teste Desenvolvimento
    Commit II -> Controller

--- src/br/com/testedesenvolvimento/controller/FiliaisController.java
+++ src/br/com/testedesenvolvimento/controller/FiliaisController.java
@@ -4,6 +4,8 @@
 package br.com.testedesenvolvimento.controller;
 
 import br.com.testedesenvolvimento.dao.*;
+import br.com.testedesenvolvimento.dao.FiliaisDao;
+import br.com.testedesenvolvimento.dao.FiliaisDaoImp;
 import br.com.testedesenvolvimento.model.*;
 
 import java.util.List;
@@ -15,8 +17,49 @@ import javax.faces.model.ListDataModel;
  * @author VictorSampaio
  *
  */
+
+@ManagedBean
+@SessionScoped
 public class FiliaisController {
-	
+
+	private Filiais filiais;
+	private DataModel listaFiliais;
+	 
+	public DataModel getListarFiliais() {
+	List<filiais> lista = new FiliaisDaoImp().list();
+	listaFiliais = new ListDataModel(lista);
+	return listaFiliais;
+	}
+	public Filiais getFiliais() {
+	return filiais;
+	}
+	public void setFiliais(Filiais filiais) {
+	this.filiais = filiais;
+	}
+	public String prepararAdicionarFiliais(){
+	filiais = new Filiais();
+	return "gerenciarFiliais";
+	}
+	public String prepararAlterarFiliais(){
+	filiais = (Filiais)(listaFiliais.getRowData());
+	return "gerenciarLivro";
+	}
+	public String excluirFiliais(){
+	Filiais filiaisTemp = (Filiais)(listaFiliais.getRowData());
+	FiliaisDao dao = new FiliaisDaoImp();
+	dao.remove(filiaisTemp);
+	return "index";
+	}
+	public String adicionarFiliais(){
+	FiliaisDao dao = new FiliaisDaoImp();
+	dao.save(filiais);
+	return "index";
+	}
+	public String alterarFiliais(){
+	FiliaisDao dao = new FiliaisDaoImp();
+	dao.update(filiais);
+	return "index";
+	}
 	
 
 }
